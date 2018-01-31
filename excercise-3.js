cb650f52e1c84d339f53bc1f0ee2d92bdb50931e
bollen <david.boll@me.com>
2018-01-31 10:59:45 +0100

    converted code from ES5 to ES6

--- excercise-3.js
+++ excercise-3.js
@@ -1,5 +1,17 @@
 /*jshint esversion: 6 */
 
+/* What is this? Does it have an equivalent in Ruby?
+
+The 'this' keyword doesn't really have a short answer. But it kindof
+relates to the function or scope where it is called. So in the
+example below, if we crete a new Person like "david", insted of
+writing david.first we can write this.first.
+
+It's equivalent in Ruby would be the 'self' keyword. Even though it's
+not exactly the same.
+
+*/
+
 class Person {
   constructor(first, last) {
     this.first = first;
